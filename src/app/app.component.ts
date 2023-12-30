02a2654622c4c2a53e40494554abf0ee40a626a9
Gheorghe Dandiș <dvndis.gheorghe@gmail.com>
2023-12-31 01:12:42 +0200

    Project initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,139 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { LyricsService } from "./services/LyricsService/lyrics.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <form [formGroup]="searchForm" (onSubmit)="search()">
+      <input type="text" placeholder="song_name" formControlName="song_name" />
+      <br />
+      <input
+        type="text"
+        placeholder="song_artist"
+        formControlName="song_artist"
+      />
+      <br />
+      <button (click)="search()">Search</button>
+    </form>
 
-    <router-outlet></router-outlet>
+    <div class="information">
+      @if (this.hasError) {
+      <p class="error">{{ this.errorMessage }}</p>
+      } @else{
+      <img
+        class="image"
+        src="{{ this.lyrics?.header_image_url }}"
+        alt="{{ this.lyrics?.header_image_url }}"
+      />
+      <h1>{{ this.lyrics?.full_title }}</h1>
+      <p class="lyrics">{{ this.lyrics?.lyrics }}</p>
+      }
+    </div>
   `,
-  styles: [],
+  styles: [
+    `
+      form {
+        input[type="text"] {
+          border: 1px solid #d1d5db;
+          color: #f3f4f6;
+          font-size: 0.875rem;
+          border-radius: 0.375rem;
+          display: block;
+          padding: 0.625rem;
+          background-color: #1f2937;
+          border-color: #4b5563;
+          outline: none;
+        }
+
+        input[type="text"]::placeholder {
+          color: #a0aec0;
+        }
+
+        button {
+          border: 1px solid #d1d5db;
+          color: #f3f4f6;
+          font-size: 0.875rem;
+          border-radius: 0.375rem;
+          display: block;
+          padding: 0.625rem;
+          background-color: #1f2937;
+          border-color: #4b5563;
+          outline: none;
+          cursor: pointer;
+        }
+      }
+
+      .information {
+        margin-top: 25px;
+
+        .image {
+          width: 300px;
+        }
+
+        .lyrics {
+          white-space: pre-wrap;
+          line-height: 24px;
+        }
+
+        .error {
+          font-weight: 600;
+          color: red;
+        }
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'Lyrical';
+  title = "Lyrical";
+
+  searchForm: FormGroup;
+  lyrics: any | undefined;
+  errorMessage: string | undefined;
+  hasError: boolean | undefined;
+
+  constructor(
+    private lyricsService: LyricsService,
+    private formBuilder: FormBuilder
+  ) {
+    this.searchForm = this.formBuilder.group({
+      song_name: [null, [Validators.required]],
+      song_artist: [null, [Validators.required]],
+    });
+  }
+
+  search() {
+    console.log(
+      this.searchForm.value.song_name,
+      this.searchForm.value.song_artist
+    );
+
+    this.lyricsService
+      .getLyrics(
+        this.searchForm.value.song_name,
+        this.searchForm.value.song_artist
+      )
+      .subscribe(
+        (result) => {
+          console.log(result);
+          this.lyrics = result;
+          this.hasError = false;
+        },
+        (error) => {
+          console.log(error);
+          this.hasError = true;
+          this.errorMessage = error.error.error;
+        }
+      );
+  }
 }
---
+++ src/app/services/LyricsService/lyrics.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LyricsService {
+  private apiURL = "http://127.0.0.1:5000/get_lyrics";
+
+  constructor(private httpClient: HttpClient) {}
+
+  getLyrics(song_name: string, song_artist: string): Observable<any> {
+    const headers = new HttpHeaders({ "Content-Type": "application/json" });
+
+    const data = {
+      song_name: song_name,
+      song_artist: song_artist,
+    };
+
+    return this.httpClient.post(`${this.apiURL}`, data, { headers: headers });
+  }
+}
